02721f65147f06ae075c3a96672c18a9d345c46a
Nauzet Melián Batista <nauzet18@gmail.com>
2015-11-24 17:03:33 +0000

    ejemplo 06. Se ha añadido la capacidad de formulario y actualización de la lista. Con solo implementar la funcion save y añadir el nuevo elemento a la coleccion alumno

--- 03/alumnos.js
+++ 03/alumnos.js
@@ -1,10 +1,18 @@
-	function AlumnosController($scope)
-	{
-		$scope.alumnos = [
-      { nombre: "nauzet", curso: "1º", telefono: "925248763" },
-      { nombre: "nzet", curso: "3º", telefono: "965248763" },
-      { nombre: "nauz", curso: "4º", telefono: "915248763" },
-      { nombre: "auzet", curso: "6º", telefono: "920248763" },
-    ]
+function AlumnosController($scope)
+{
+	$scope.alumnos = [
+    { nombre: "nauzet", curso: "1º", telefono: "925248763" },
+    { nombre: "nzet", curso: "3º", telefono: "965248763" },
+    { nombre: "nauz", curso: "4º", telefono: "915248763" },
+    { nombre: "auzet", curso: "6º", telefono: "920248763" },
+  ]
 
-	}
+  $scope.Save = function(){
+
+    $scope.alumnos.push(
+        { nombre: $scope.nuevoAlumno.nombre, curso: $scope.nuevoAlumno.curso, telefono: $scope.nuevoAlumno.telefono }
+      );  
+  }
+
+
+}
