8083c98d4eb31721048c5a77ca4f40b31e7b14fa
Nauzet Melián Batista <nauzet18@gmail.com>
2015-11-24 16:13:25 +0000

    Ejemplo 03 y 04. Basicamente el uso del ng-repeat

---
+++ 03/alumnos.js
@@ -0,0 +1,10 @@
+	function AlumnosController($scope)
+	{
+		$scope.alumnos = [
+      { nombre: "nauzet", curso: "1º", telefono: "925248763" },
+      { nombre: "nzet", curso: "3º", telefono: "965248763" },
+      { nombre: "nauz", curso: "4º", telefono: "915248763" },
+      { nombre: "auzet", curso: "6º", telefono: "920248763" },
+    ]
+
+	}
