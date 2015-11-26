51851a77422c48416642a7d3b831eccc96ae6c32
Nauzet Melián Batista <nauzet18@gmail.com>
2015-11-26 12:26:46 +0000

    ejemplo 23. Se ha añadido un modelo de bloque de contenido en el controlador main y se ha maquetado en la vista del main.

--- 22a/app/scripts/controllers/main.js
+++ 22a/app/scripts/controllers/main.js
@@ -30,5 +30,24 @@ angular.module('22aApp')
       }
     ];
 
+    // Modelo para los tres bloques de contenidos
+    var baseURL='http://lorempixel.com/200/200/'
+    $scope.contenido=[
+    {
+       img:baseURL+'people',
+       title:'Sobre Nosotros',
+       sumario:'Somos una empresa comprometida con la vida sana'
+    },
+    {
+       img:baseURL+'business',
+       title:'Nuestros Servicios',
+       sumario:'Ofrecemos asesoría profesional para mantenerse Sanos, buena alimentación, cuales son los mejores ejercicios para tí, etc.'
+    },
+    {
 
+       img:baseURL+'transport',
+       title:'Contáctanos',
+       sumario:'#333, Buena vida Online, Plaza Central, Durance, Zip-432167'
+    }
+    ]
   });
