2a3c568368a90523b459ed0b98fe1fae7c721066
Nauzet Melián Batista <nauzet18@gmail.com>
2015-11-26 12:41:07 +0000

    ejemplo 25. creando contenido para la vista articilos. Se ha usado acordeon para el aspecto final.

--- 22a/app/scripts/controllers/articulos.js
+++ 22a/app/scripts/controllers/articulos.js
@@ -8,10 +8,19 @@
  * Controller of the 22aApp
  */
 angular.module('22aApp')
-  .controller('ArticulosCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  .controller('ArticulosCtrl', function ($scope) {
+    $scope.posts = [
+        {
+        title:"Alimentos buenos para la salud",
+        content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus rhoncus quam leo, id tristique sapien viverra eu. Maecenas ipsum lectus, suscipit auctor tristique in, venenatis ut nisl. Quisque eget bibendum libero. Nam nec mi augue."
+        },
+        {
+        title:"Alimentos menos buenos para la salud",
+        content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus rhoncus quam leo, id tristique sapien viverra eu. Maecenas ipsum lectus, suscipit auctor tristique in."
+        },
+        {
+        title:"¡¡¡Limita tu cantidad de hidratos de carbono",
+        content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus rhoncus quam leo, id tristique sapien viverra eu. Maecenas ipsum lectus, suscipit auctor tristique in, venenatis ut nisl. Quisque eget bibendum libero. Nam nec mi augue."
+      }
     ];
   });
